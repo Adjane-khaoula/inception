4c0099fa7dade202e989c5ab0a21f406653e1fd9
Adjane-khaoula <khawlaadjane@gmail.com>
2023-09-11 06:48:10 +0100

    fix volumes

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,6 +1,6 @@
-mkdir /var/www/
-mkdir /var/www/html
-mkdir /var/www/html/wordpress
+# mkdir /var/www/
+# mkdir /var/www/html
+# mkdir /var/www/html/wordpress
 
 # cd /var/www/html
 
@@ -11,7 +11,7 @@ wp core download --allow-root
 # Creating the wp-config.php file using this command.
 wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
 # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
-wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a
+wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
 exec $@
 
 
