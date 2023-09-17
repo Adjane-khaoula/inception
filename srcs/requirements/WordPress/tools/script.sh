4f9e1462953f6e6fe4589a18d1993bc2eb06a0fe
Adjane-khaoula <khawlaadjane@gmail.com>
2023-09-17 20:33:13 +0100

    handl somme errors

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -14,7 +14,7 @@ wp core download --allow-root
 # 	--dbuser=$SQL_USER \
 # 	--dbpass=$SQL_PASSWORD \
 # 	--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
-mv wp-config-sample.php  wp-config.php 
+mv wp-config-sample.php  wp-config.php
 sed -i "s/database_name_here/$SQL_DATABASE/1" wp-config.php
 sed -i "s/username_here/$SQL_USER/1" wp-config.php
 sed -i "s/password_here/$SQL_PASSWORD/g" wp-config.php
@@ -24,4 +24,6 @@ sed -i "s/localhost/$SQL_HOST/g" wp-config.php
 # wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
 # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
 wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${Word_USER} --admin_password=${Word_PW} --admin_email=${Word_EMAIL} --allow-root
-exec $@
+php-fpm7.4 -R -F
+# exec $@
+
