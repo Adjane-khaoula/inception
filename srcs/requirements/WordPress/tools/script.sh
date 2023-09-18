825670e24ca8df0fa9585a4204a31a803b726dc0
Adjane-khaoula <khawlaadjane@gmail.com>
2023-09-18 08:11:11 +0100

    wordpress work

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-
-# # cd /var/www/html
-# # mkdir /var/www/
-# # mkdir /var/www/html
 sleep 10
 
 cd /var/www/html/wordpress
 rm -rf *
 
 wp core download --allow-root
-# wp core create	--allow-root \
-# 	--dbname=$SQL_DATABASE \
-# 	--dbuser=$SQL_USER \
-# 	--dbpass=$SQL_PASSWORD \
-# 	--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+
 mv wp-config-sample.php  wp-config.php
 sed -i "s/database_name_here/$SQL_DATABASE/1" wp-config.php
 sed -i "s/username_here/$SQL_USER/1" wp-config.php
 sed -i "s/password_here/$SQL_PASSWORD/g" wp-config.php
 sed -i "s/localhost/$SQL_HOST/g" wp-config.php
 
-# Creating the wp-config.php file using this command.
-# wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
-# Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
 wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${Word_USER} --admin_password=${Word_PW} --admin_email=${Word_EMAIL} --allow-root
+wp user create $Word_USER $WORDPRESS_EMAIL --role=author --user_pass=$Word_PW --allow-root
 php-fpm7.4 -R -F
-# exec $@
 
