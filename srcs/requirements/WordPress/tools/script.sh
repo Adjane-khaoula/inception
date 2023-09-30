db76fe71086ad7075b9ef62292091965dd9dfafd
Adjane-khaoula <khawlaadjane@gmail.com>
2023-09-30 05:58:29 +0100

    handl somme errors

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -14,12 +14,9 @@ sed -i "s/localhost/$SQL_HOST/g" wp-config.php
 
 wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${Word_USER} --admin_password=${Word_PW} --admin_email=${Word_EMAIL} --allow-root
 php-fpm7.4 -R -F
-
-# Check if the 'kadjane' user exists.
 if wp user list --field=user_login --allow-root | grep -q $user_name; then
   echo "The $user_name user already exists."
 else
-  # If the 'kadjane' user doesn't exist, create it.
   wp user create $user_name $user_EMAIL --role=author --user_pass=$user_pw --allow-root
   echo "The 'kadjane' user has been created."
 fi
